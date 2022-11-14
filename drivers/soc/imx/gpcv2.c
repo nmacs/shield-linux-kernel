foundriesio/linux

commit c706fb45b357f4f63826b890732a3ed0da184ebf

Revert "soc: imx: gpcv2: allow domains without power-sequence control"
This reverts commit c0ce75395f8d088ba56dcec3218c628ef2bb6d73.

 Conflicts:
	drivers/soc/imx/gpcv2.c

Signed-off-by: Daiane Angolini <daiane.angolini@foundries.io>

drivers/soc/imx/gpcv2.c
@@ -514,6 +514,13 @@ static int imx_pgc_domain_probe(struct platform_device *pdev)
 		imx_pgc_put_clocks(domain);
 	}
 
+	return 0;
+
+out_genpd_remove:
+	pm_genpd_remove(&domain->genpd);
+out_domain_unmap:
+	pm_runtime_disable(domain->dev);
+
 	return ret;
 }
 
@@ -524,6 +531,8 @@ static int imx_pgc_domain_remove(struct platform_device *pdev)
 	of_genpd_del_provider(domain->dev->of_node);
 	pm_genpd_remove(&domain->genpd);
 	imx_pgc_put_clocks(domain);
+
+	pm_runtime_disable(domain->dev);
 
 	return 0;
 }

nmacs/shield-linux-kernel

commit bb6e70bff1b71811dceab755e4dcb8f8bb2b5107

drivers/soc/imx/gpcv2.c: complete patch revert
The revert resolution of commit c0ce75395f8d ("soc: imx: gpcv2: allow
domains without power-sequence control") left a unused error label:

| drivers/soc/imx/gpcv2.c: In function ‘imx_pgc_domain_probe’:
| drivers/soc/imx/gpcv2.c:519:1: warning: label ‘out_genpd_remove’ defined but not used [-Wunused-label]
|   519 | out_genpd_remove:
|       | ^~~~~~~~~~~~~~~~

Looks like we can savely remove this, but should return 'ret', rather
than '0' to report a possible error.

Fixes: c706fb45b357 ("Revert "soc: imx: gpcv2: allow domains without power-sequence control"")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

drivers/soc/imx/gpcv2.c
@@ -514,11 +514,6 @@ static int imx_pgc_domain_probe(struct platform_device *pdev)
 		imx_pgc_put_clocks(domain);
 	}
 
-	return 0;
-
-out_genpd_remove:
-	pm_genpd_remove(&domain->genpd);
-
 	return ret;
 }
 